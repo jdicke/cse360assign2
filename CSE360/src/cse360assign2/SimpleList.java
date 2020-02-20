e4b00ec7c4c035dc10bd7b6657b878d05a87ae83
jdicke <jojojojodicke@gmail.com>
2020-02-20 15:07:22 -0700

    Second Version. Includes:   - append(int)   - first()   - size()   - And new functionality to add(int)   and remove(int)

--- CSE360/src/cse360assign2/SimpleList.java
+++ CSE360/src/cse360assign2/SimpleList.java
@@ -1,6 +1,7 @@
 /*
  * @author Joseph Dicke
  * 169
+ * https://github.com/jdicke/cse360assign2/tree/master/CSE360/src/cse360assign2
  * Assignment #1 - This file contains the methods SimpleList(), add(int),
  * remove(), count(), toString(), and search(int num) which are all used
  * to do certain things with an int array of length 10.
@@ -16,6 +17,11 @@
  * 	String toString() - Returns a string with all of the numbers in the list
  * 	int search(num) - This method returns the index of a number in the list. 
  * 		If the number is not in the list it will return -1.
+ *  int first() - Returns the first element in the list
+ *  int size() - Returns the current number of possible locations in the list
+ *  void append(int) - Appends an integer to the end of the list and if 
+ *  	the list is full will increase the size by 50% so there will be room
+ *  	and also increments the count.
  */
 
 package cse360assign2;
@@ -24,7 +30,7 @@ public class SimpleList {
 
 	private int [] list;
 	private int count;
-	private final int SIZE = 10;
+	private int SIZE;
 	
 	/**
 	 * Class constructor initializes list to a int array of size 10. It also
@@ -32,6 +38,7 @@ public class SimpleList {
 	 */
 	public SimpleList()
 	{
+		SIZE = 10;
 		list = new int[SIZE];
 		count = 0;
 	}
@@ -39,11 +46,32 @@ public class SimpleList {
 	/**
 	 * This method adds an integer at the beginning of the int array. It will also
 	 * shift the rest of the integers, if any, in the list to the right.
+	 * If the list is full it will increase the size by 50% so there will be room.
 	 *
 	 * @param  num  an integer to add to the list
 	 */
 	public void add(int num)
 	{
+		if (count == SIZE)
+		{
+			// List is full so expand the list by 50% first
+			int [] temp_list = new int[SIZE];
+			
+			for ( int iterator = 0; iterator < SIZE; iterator++ )
+			{
+				temp_list[ iterator ] = list[ iterator ];
+			}
+			
+			int OLD_SIZE = SIZE;
+			SIZE = ( int ) ( SIZE + ( SIZE*0.5 ) );
+			
+			list = new int[ SIZE ];
+			
+			for ( int iterator = 0; iterator < OLD_SIZE; iterator++ )
+			{
+				list[ iterator ] = temp_list[ iterator ];
+			}
+		}
 		count++;
 		int previousValue = list[ 0 ];
 		
@@ -59,11 +87,31 @@ public class SimpleList {
 	/**
 	 * This method removes an integer from the list if it exists. If it does exist then
 	 * it will remove it and shift the rest of the integers after that number to the left.
+	 * If the list has more than 25% empty places, then the size of the list decreases.
 	 *
 	 * @param  num  an integer to remove from the list
 	 */
 	public void remove(int num)
 	{
+		// Check if the list has more than 25% empty places
+		if ( ( 1 - (count / SIZE) ) > 0.25 )
+		{
+			int [] temp_list = new int[ SIZE ];
+			
+			for ( int iterator = 0; iterator < SIZE; iterator++ )
+			{
+				temp_list[ iterator ] = list[ iterator ];
+			}
+			
+			SIZE--;
+			
+			list = new int[ SIZE ];
+			
+			for ( int iterator = 0; iterator < SIZE; iterator++ )
+			{
+				list[ iterator ] = temp_list[ iterator ];
+			}	
+		}
 		int index = 0, innerCounter = 0;
 		int[] temp = new int [ SIZE ] ;
 		
@@ -131,5 +179,74 @@ public class SimpleList {
 			}
 		}
 		return index;
+	}
+	
+	/**
+	 * This method appends an integer to the end of the list.
+	 * If the list is full it will increase the size by 50% so there will be room.
+	 *
+	 * @param  num  an integer to append to the list
+	 */
+	public void append(int num)
+	{
+		if (count == SIZE)
+		{
+			// List is full so expand the list by 50% first
+			int [] temp_list = new int[SIZE];
+			
+			for ( int iterator = 0; iterator < SIZE; iterator++ )
+			{
+				temp_list[ iterator ] = list[ iterator ];
+			}
+			
+			SIZE = ( int ) ( SIZE + ( SIZE*0.5 ) );
+			
+			list = new int[ SIZE ];
+			
+			for ( int iterator = 0; iterator < SIZE; iterator++ )
+			{
+				list[ iterator ] = temp_list[ iterator ];
+			}
+		}
+		count++;
+		int positionToEnter = 0;
+		
+		for (int iterator = 0; iterator < SIZE; iterator++)
+		{
+			if ( list [ iterator ] == 0 )
+			{
+				positionToEnter = iterator;
+			}
+			list[ positionToEnter ] = num;
+		}
+	}
+	
+	/**
+	 * This method will return the first element in the list.
+	 *
+	 * @param  none
+	 */
+	public int first()
+	{
+		return list[0];
+	}
+	
+	/**
+	 * This method return the current number of possible locations
+	 * in the list
+	 *
+	 * @param  none
+	 */
+	public int size()
+	{
+		int amountOfPossibleLocations = 0;
+		for ( int iterator = 0; iterator < SIZE; iterator++ )
+		{
+			if ( list [ iterator ] == 0 )
+			{
+				amountOfPossibleLocations++;
+			}
+		}
+		return amountOfPossibleLocations;
 	}
 }
